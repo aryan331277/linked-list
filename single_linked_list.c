a006df02d3972cd4669d3c1af9e4e9e5ff1195ca
aryan331277 <bt24csd009@iiitn.ac.in>
2025-03-12 21:31:55 +0530

    Update single_linked_list.c

--- single_linked_list.c
+++ single_linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Node{// defines a new data type called as Node
     int data;
     struct Node* next;//a pointer to another Node structure. Linking all.
@@ -7,11 +8,21 @@ struct Node{// defines a new data type called as Node
 
 struct Node* createNode(int data){// declaring a function that takes data and returns a pointer to Node.
     struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));//Allocates memory to Node structure
-    newNode->data=data;
-    newNode->next=NULL;
+    newNode->data=data;// Store the passed data in the node.
+    newNode->next=NULL;// Initialize the next pointer to NULL (no next node yet).
     return newNode;//returns the pointer to the New Node
 }
-int main() {
 
+void printList(struct Node* head){
+    struct Node* current=head;
+    while(current!=NULL){
+        printf("%d->",current->data);
+        current=current->next;
+    }
+}
+
+int main() {
+    struct Node* node=NULL;
+    printList(head);
     return 0;
 }
